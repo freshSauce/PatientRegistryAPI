80de06149247a7d44a51169b4f83f719b94797db
freshSauce <nex0loke@gmail.com>
2024-09-27 23:24:49 -0600

    fix: remove example endpoints

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
 from routers import patients_router
 
-app = FastAPI(title="Patient Registry API", description="API that allows intercomunication with health centres, "
+app = FastAPI(title="Patient Registry API", description="API that allows intercomunication with healthcare centres, "
                                                         "storing patients information in one single database")
 app.include_router(patients_router)
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return RedirectResponse("/docs")
